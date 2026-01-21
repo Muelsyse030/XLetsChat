9e7747a2f06e24aeadb228cf003b63e2eae28007
Muelsyse030 <15279290607@163.com>
2026-01-21 14:06:07 +0800

    实现消息发送全链路闭环

--- server/gateway/main.cc
+++ server/gateway/main.cc
@@ -7,18 +7,83 @@
 #include <grpcpp/channel.h>
 #include <grpcpp/client_context.h>
 #include <grpcpp/create_channel.h>
+#include <thread>
 
 std::unique_ptr<im::LogicService::Stub> logic_stub;
 
-// 定义每个连接的私有数据 (Session)
+
 struct PerSocketData {
-    int64_t uid = 0; // 登录后记录 UID
+    int64_t uid = 0; 
 };
+
+class SessionManager{
+    public:
+    static SessionManager& GetInstance(){
+        static SessionManager instance;
+        return instance;
+    }
+    void AddSession(int64_t uid, uWS::WebSocket<false, true, PerSocketData>* ws){
+        std::lock_guard<std::mutex> lock(mutex_);
+        sessions_[uid] = ws;
+    }
+
+    void RemoveSession(int64_t uid){
+        std::lock_guard<std::mutex> lock(mutex_);
+        if(sessions_.find(uid) != sessions_.end()){
+            sessions_.erase(uid);
+        }
+    }
+    bool PushToUser(int64_t uid, const std::string& data) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        auto it = sessions_.find(uid);
+        if (it != sessions_.end()) {
+            it->second->send(data, uWS::OpCode::BINARY);
+            return true;
+        }
+        return false;
+    }
+    private:
+    std::mutex mutex_;
+    std::unordered_map<int64_t, uWS::WebSocket<false, true, PerSocketData>*> sessions_;
+};
+class GatewayServiceImpl final : public im::GatewayService::Service{
+    grpc::Status PushMsg(grpc::ServerContext* context , const im::PushMsgReq* request , im::PushMsgRes* reply){
+        spdlog::info("RPC PushMsg recv : ToUId = {} , len = {}",request->to_uid() , request->content());
+        bool success = SessionManager::GetInstance().PushToUser(request->to_uid() , request->content());
+        if(success){
+            reply->set_err_code(0);
+            spdlog::info("<<< pushed to client successfully");
+        }
+        else{
+            reply->set_err_code(-1);
+            reply->set_err_msg("User not found locally");
+            spdlog::warn("<<<Push to User {} not found on this gateway" , request->to_uid());
+        }
+        return grpc::Status::OK;
+    }
+};
+
+void RunGrpcServer(){
+        std::string server_address = "0.0.0.0:50052";
+        GatewayServiceImpl service;
+
+         grpc::ServerBuilder builder;
+        builder.AddListeningPort(server_address , grpc::InsecureServerCredentials());
+        builder.RegisterService(&service);
+
+        std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+        spdlog::info("Gateway gRpc Server listening on {}", server_address);
+        server->Wait();
+    }
 
 int main() {
 
     spdlog::set_pattern("[%H:%M:%S %z] [%^%L%$] [Gateway-uWS] %v");
     spdlog::info("Starting uWebSockets Gateway on port 8000...");
+
+    std::thread grpc_thread(RunGrpcServer);
+    grpc_thread.detach();
+
     //初始化gRpc Channel
     std::string logic_server_address = "0.0.0.0:50051";
     auto channel = grpc::CreateChannel(logic_server_address , grpc::InsecureChannelCredentials());
@@ -27,19 +92,15 @@ int main() {
 
     uWS::App()
         .ws<PerSocketData>("/ws", {
-            // 1. 配置项
+            
             .compression = uWS::SHARED_COMPRESSOR,
-            .maxPayloadLength = 16 * 1024, // 最大包体 16KB
-            .idleTimeout = 60,             // 60秒无心跳断开
+            .maxPayloadLength = 16 * 1024, 
+            .idleTimeout = 60,             
 
-            // 2. 连接建立 (Open)
             .open = [](auto *ws) {
                 spdlog::info("New Connection!");
             },
-
-            // 3. 收到消息 (Message) - 核心逻辑
             .message = [](auto *ws, std::string_view message, uWS::OpCode opCode) {
-                // 必须是二进制消息
                 if (opCode != uWS::OpCode::BINARY) {
                     spdlog::warn("Drop non-binary message");
                     return;
@@ -50,15 +111,13 @@ int main() {
                     spdlog::warn("Packet too short: {}", message.length());
                     return;
                 }
-                // 解析 Header (注意：uWS 的 message 是 string_view，可以直接转 const uint8_t*)
                 const uint8_t* buffer = reinterpret_cast<const uint8_t*>(message.data());
                 PacketHeader header = PacketHelper::DecodeHeader(buffer);
-                //处理登录请求(0x1001)
+                
                 if(header.cmd_id == 0x1001){
                     im::LoginReq req;
                     if(req.ParseFromArray(buffer + HEADER_LEN , header.length - HEADER_LEN)){
                         spdlog::info(">>recv logincReq: uid = {}" , req.uid());
-                        //调用logic server的Login接口
                         im::LoginRes res;
                         grpc::ClientContext context;
                         grpc::Status status = logic_stub->Login(&context , req , &res);
@@ -66,6 +125,7 @@ int main() {
                             if(res.err_code() == im::ErrorCode::ERR_SUCCESS){
                                 spdlog::info("<<< rpc login success ! Session_id = {}" , res.session_id());
                                 ws->getUserData()->uid = req.uid();
+                                SessionManager::GetInstance().AddSession(req.uid() , ws);
                             }
                             else{
                                 spdlog::warn("<<< RPC Login Failed: {}", res.err_msg());
@@ -92,12 +152,53 @@ int main() {
                         }
                     }   
                 }
+                else if(header.cmd_id == 0x1003){
+                    im::MsgSendReq req;
+                    if((message.length() >= HEADER_LEN + (header.length - HEADER_LEN)) &&
+                        req.ParseFromArray(buffer + HEADER_LEN , message.length() - HEADER_LEN)){
+                            int64_t current_id = ws->getUserData()->uid;
+                            if(current_id == 0){
+                                spdlog::warn("User not logged in , frop msg");
+                                return;
+                            }
+                            req.mutable_msg()->set_from_uid(current_id);
+                            spdlog::info(">> Recv MsgSendReq: to {} content = {}" ,req.msg().to_uid() , req.msg().content());
+
+                            im::MsgSendRes res;
+                            grpc::ClientContext context;
+                            grpc::Status status = logic_stub->SendMsg(&context , req , &res);
+                            if(status.ok()){
+                                std::string res_body;
+                                res.SerializeToString(&res_body);
+
+                                PacketHeader res_header;
+                                res_header.length = HEADER_LEN + res_body.size();
+                                res_header.version = header.version;
+                                res_header.cmd_id = 0x1004;
+                                res_header.seq_id = header.seq_id;
+
+                                uint8_t head_buf[HEADER_LEN];
+                                PacketHelper::EncodeHeader(res_header , head_buf);
+
+                                std::string send_data;
+                                send_data.append((char*)head_buf , HEADER_LEN);
+                                send_data.append(res_body);
+
+                                ws->send(send_data , uWS::OpCode::BINARY);
+                                spdlog::info("<<< Reply MsgSendRes : OK");
+                            }
+                            else{
+                                spdlog::error("RPC SendMsg Failed: {}" , status.error_message());
+                            }
+                        }
+                }
             },
             .close = [](auto *ws, int code, std::string_view message) {
+                SessionManager::GetInstance().RemoveSession(ws->getUserData()->uid);
                 spdlog::info("Connection closed. UID={}", ws->getUserData()->uid);
             }
         })
-        // 监听端口
+        
         .listen(8000, [](auto *listen_socket) {
             if (listen_socket) {
                 spdlog::info("Listening on port 8000 successfully");
@@ -106,7 +207,7 @@ int main() {
                 exit(-1);
             }
         })
-        // 启动事件循环 (阻塞)
+        
         .run();
     return 0;
 }
--- server/logic_server/main.cc
+++ server/logic_server/main.cc
@@ -7,6 +7,9 @@
 
 #include "im.pb.h"
 #include "im_service.grpc.pb.h"
+#include "redis_client.h"
+#include <mutex>
+#include <unordered_map>
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -15,17 +18,58 @@ using grpc::Status;
 using im::LogicService;
 using im::LoginReq;
 using im::LoginRes;
+using im::MsgSendReq; 
+using im::MsgSendRes;
+
+std::string PackPushMsg(const im::ChatMsg& chat_msg) {
+
+    im::MsgPush push_pkg;
+    *push_pkg.mutable_msg() = chat_msg;
+    
+    std::string body_bytes;
+    push_pkg.SerializeToString(&body_bytes);
+
+    uint32_t total_len = 12 + body_bytes.size();
+    uint16_t version = 1;
+    uint16_t cmd_id = 0x1005;
+    uint32_t seq_id = 0;      
+
+    uint32_t net_len = htonl(total_len);
+    uint16_t net_ver = htons(version);
+    uint16_t net_cmd = htons(cmd_id);
+    uint32_t net_seq = htonl(seq_id);
+
+    std::string packet;
+    packet.append((char*)&net_len, 4);
+    packet.append((char*)&net_ver, 2);
+    packet.append((char*)&net_cmd, 2);
+    packet.append((char*)&net_seq, 4);
+    packet.append(body_bytes);
+
+    return packet;
+}
 
 class LogicServiceImpl final : public LogicService::Service{
+public:
+    LogicServiceImpl(RedisClient* redis) : redis_(redis){}
+
     Status Login(ServerContext* context , const LoginReq* request , LoginRes* reply) override {
         spdlog::info("PRC Login Request: Uid= {} , token = {} , device = {}" , request->uid() , request->token() , request->device_id());
 
         if(request->token() == "123456"){
-            //登录成功
             reply->set_err_code(im::ErrorCode::ERR_SUCCESS);
             reply->set_session_id("sess_" + std::to_string(request->uid())); //生成一个假session
             reply->set_server_time(time(nullptr));
             spdlog::info("->Login Success: uid = {}" , request->uid());
+            std::string redis_key = "IM:USER:SESS:" + std::to_string(request->uid());
+            std::string gateway_addr = "0.0.0.0:50052";
+            if(redis_->Set(redis_key , gateway_addr)){
+                spdlog::info("->Saved session to Redis: {} -> {}" , redis_key , gateway_addr);
+            }
+            else{
+                spdlog::error("->Write Redis Failed!");
+            }
+            spdlog::info("-> Login Success: UID={}", request->uid());
         }
         else{
             reply->set_err_code(im::ErrorCode::ERR_AUTH_FAIL);
@@ -34,11 +78,61 @@ class LogicServiceImpl final : public LogicService::Service{
         }
         return Status::OK;
     }
+    Status SendMsg(ServerContext* context , const MsgSendReq* request , MsgSendRes* reply) override {
+        const auto& msg = request->msg();
+        spdlog::info("RPC sendMsg: from={} to={} content={}" , msg.from_uid() , msg.to_uid() , msg.content());
+        
+        int64_t msg_id = std::chrono::system_clock::now().time_since_epoch().count();
+       
+        std::string redis_key = "IM:USER:SESS:" + std::to_string(msg.to_uid());
+        auto gateway_addr_opt = redis_->Get(redis_key);
+
+        if(gateway_addr_opt.has_value()){
+            std::string gateway_addr = gateway_addr_opt.value();
+            spdlog::info("->Found target user {} at gateway[{}]" , msg.to_uid() , gateway_addr_opt.value());
+            auto channel = grpc::CreateChannel(gateway_addr , grpc::InsecureChannelCredentials());
+            auto stub = im::GatewayService::NewStub(channel);
+
+            im::PushMsgReq push_req;
+            push_req.set_to_uid(msg.to_uid());
+
+            std::string raw_packet = PackPushMsg(msg);
+            push_req.set_content(raw_packet);
+
+            im::PushMsgRes push_res;
+            grpc::ClientContext client_context;
+            client_context.set_deadline(std::chrono::system_clock::now() + std::chrono::seconds(2));
+
+            grpc::Status status = stub->PushMsg(&client_context , push_req , &push_res);
+            
+            if (status.ok() && push_res.err_code() == 0) {
+                spdlog::info("--> Push to Gateway Success!");
+            } else {
+                spdlog::warn("--> Push to Gateway Failed: {} ({})", status.error_message(), push_res.err_msg());
+            }
+        }
+        else{
+            spdlog::warn("->Target user {} is offline(reids key not found)" , msg.to_uid());
+
+        }
+        reply->set_err_code(im::ErrorCode::ERR_SUCCESS);
+        reply->set_msg_id(msg_id);
+        reply->set_create_time(time(nullptr));
+
+        return Status::OK;
+    }
+    private:
+        RedisClient* redis_;
 };
 
 void RunServer(){
     std::string server_address("0.0.0.0:50051");
-    LogicServiceImpl service;
+    RedisClient redis;
+    if(!redis.connect("0.0.0.0" , 6379 , "redis_pwd_123")){
+        spdlog::error("Failed to connect to Redis. Exiting.");
+        return;
+    }
+    LogicServiceImpl service(&redis);
 
     ServerBuilder builder;
     builder.AddListeningPort(server_address , grpc::InsecureServerCredentials());
